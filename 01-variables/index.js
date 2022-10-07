9890bb4d18f4cf9956b976bf1ff9ff4dc5df5b00
Pedro Higuera <pahf33@gmail.com>
2022-10-06 19:25:37 -0500

    class 1

---
+++ 01-variables/index.js
@@ -0,0 +1,32 @@
+// esto es un comentario de una linea
+
+/* 
+esto es un comentario de una linea
+esto es un comentario de una linea
+esto es un comentario de una linea
+esto es un comentario de una linea
+esto es un comentario de una linea
+esto es un comentario de una linea
+esto es un comentario de una linea
+*/
+
+let nombre = "Pedro"; // inicializar
+nombre = "John"; // asignar
+
+let nombre2 = "John"; // inicializar
+let nombre3 = "Pablo"; // inicializar
+
+let nombre4 = "Rosa",
+  nombre5 = "Jose";
+
+const edad = 27; // inicializar
+let esMayorDeEdad = true;
+
+let direccion; // declarar
+direccion = "avenida bla bla bla....";
+
+const dni = 111111; // variables tipo const solo se pueden inicializar
+const telefono = "+57 12334556";
+
+const nombreCompleto = nombre + " " + nombre3;
+console.log(nombreCompleto);
---
+++ 01-variables/proyecto/index.js
@@ -0,0 +1,10 @@
+const username = prompt("Ingrese el username");
+const password = prompt("Ingrese la contrasena");
+
+const mensaje = "Su token es: " + username + password;
+
+alert(mensaje);
+
+//const edad = parseInt(prompt("Ingrese la edad"));
+
+//console.log("La edad es ", parseFloat(edad));
